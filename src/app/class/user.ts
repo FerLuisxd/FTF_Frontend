0e1898b9c2946b6ebee87f5d3a31ed145431c5a9
Alexander Benavides <ferluisxd@gmail.com>
2020-03-02 16:46:09 -0500

    feat: added user

---
+++ src/app/class/user.ts
@@ -0,0 +1,25 @@
+export class User {
+    id: number;
+  
+    enabled:boolean
+  
+    activated:boolean
+    
+    name:string
+
+    username:string
+  
+
+    email:string
+  
+
+    usergroup:string
+
+    lastVisit:Date
+  
+
+    registered:Date
+  
+    deleted:boolean
+  
+  }
